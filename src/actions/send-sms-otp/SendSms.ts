7f56ac94b19e05d2d15186b8fa0265a9b2a323d1
Tanim Sk <sktanim5800@gmail.com>
2025-03-31 00:55:40 +0600

    Update SendSms.ts

--- src/actions/send-sms-otp/SendSms.ts
+++ src/actions/send-sms-otp/SendSms.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 import { toast } from "sonner";
 import { z } from "zod";
 
-const englishRegex = /^[a-zA-Z0-9\s.,!?'"()@#$%^&*-+=:;_/]*$/;
+// const englishRegex = /^[a-zA-Z0-9\s.,!?'"()@#$%^&*-+=:;_/]*$/;
+const englishRegex = /^.*$/;
 const SendSmsSchema = z.object({
   message: z
     .string()
