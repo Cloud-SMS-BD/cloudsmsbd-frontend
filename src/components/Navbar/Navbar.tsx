b2a9aec2cade684a606b1c7d1dfbd2a7f3454592
Tofaal9152 <tofaal91522@gmail.com>
2025-04-19 13:17:07 +0600

    add notification [maintenance]

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -11,6 +11,7 @@ import {
 import Link from "next/link";
 import { ModeToggle2 } from "../ui/ModeToggle2";
 import MobileNavbar from "./MobileNavar";
+import Notification from "../notifications/notification";
 
 const Navbar = () => {
   const isLogin = useAppSelector(selectIsLogin);
@@ -33,55 +34,58 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="z-50 bg-opacity-30 backdrop-blur-lg sticky top-0 p-4 border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700 transition-colors">
-      <div className="container mx-auto flex items-center justify-between lg:px-8">
-        {/* Logo */}
-        <div className="flex items-center space-x-4">
-          <MobileNavbar navItems={navItems} />
-          <Link href="/" className="flex items-center space-x-2">
-            <img
-              src="/logo.jpg"
-              alt="logo"
-              className="rounded-full md:block hidden size-8 md:size-12 border-2 border-sky-500"
-              width={50}
-              height={50}
-            />
-            <p className="text-xl font-bold text-gray-700 dark:text-gray-300">
-              Cloud <span className="text-sky-500">SMS BD</span>
-            </p>
-          </Link>
-        </div>
-
-        {/* Desktop Nav Items */}
-        <ul className="hidden md:flex space-x-6">
-          {navItems.map((item, index) => (
-            <li key={index}>
-              <Link
-                href={item.link}
-                className="hover:underline flex items-center space-x-1 font-semibold"
-              >
-                {item.icon} <span>{item.name}</span>
-              </Link>
-            </li>
-          ))}
-        </ul>
-
-        {/* Login || Profile || MobileNav */}
-        <div className="flex items-center space-x-4">
-          <ModeToggle2 />
-          {isLogin ? (
-            <Profile />
-          ) : (
-            <Link href="/auth/login">
-              <div className="relative flex space-x-2 items-center z-10 rounded-full bg-sky-500 text-white py-[6.5px] px-6">
-                <LogInIcon size={16} />
-                <span>Login</span>
-              </div>
+    <section>
+      <Notification show={true}/>
+      <nav className="z-50 bg-opacity-30 backdrop-blur-lg sticky top-0 p-4 border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700 transition-colors">
+        <div className="container mx-auto flex items-center justify-between lg:px-8">
+          {/* Logo */}
+          <div className="flex items-center space-x-4">
+            <MobileNavbar navItems={navItems} />
+            <Link href="/" className="flex items-center space-x-2">
+              <img
+                src="/logo.jpg"
+                alt="logo"
+                className="rounded-full md:block hidden size-8 md:size-12 border-2 border-sky-500"
+                width={50}
+                height={50}
+              />
+              <p className="text-xl font-bold text-gray-700 dark:text-gray-300">
+                Cloud <span className="text-sky-500">SMS BD</span>
+              </p>
             </Link>
-          )}
+          </div>
+
+          {/* Desktop Nav Items */}
+          <ul className="hidden md:flex space-x-6">
+            {navItems.map((item, index) => (
+              <li key={index}>
+                <Link
+                  href={item.link}
+                  className="hover:underline flex items-center space-x-1 font-semibold"
+                >
+                  {item.icon} <span>{item.name}</span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+
+          {/* Login || Profile || MobileNav */}
+          <div className="flex items-center space-x-4">
+            <ModeToggle2 />
+            {isLogin ? (
+              <Profile />
+            ) : (
+              <Link href="/auth/login">
+                <div className="relative flex space-x-2 items-center z-10 rounded-full bg-sky-500 text-white py-[6.5px] px-6">
+                  <LogInIcon size={16} />
+                  <span>Login</span>
+                </div>
+              </Link>
+            )}
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </section>
   );
 };
 
---
+++ src/components/notifications/notification.tsx
@@ -0,0 +1,16 @@
+const Notification = ({ show }: { show: boolean }) => {
+  return (
+    <div
+      className={`${
+        show
+          ? "bg-red-500 text-white text-center py-2 px-4 font-medium shadow-md dark:bg-red-600 dark:text-gray-100"
+          : "hidden"
+      }`}
+    >
+      ⚠️ Our website is undergoing maintenance and will be unavailable until
+      April 22, 2025, 11:59 PM. We apologize for the inconvenience.
+    </div>
+  );
+};
+
+export default Notification;
